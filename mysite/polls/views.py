1f0c4666c6f9506dbaf2bb1e92619a64cb79d789
Ante Perisic <perisic.ante@gmail.com>
2021-02-14 12:55:09 +0100

    added new views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import loader
 from django.http import HttpResponse
 import sys
-import datetime
+import datetime as mydt
 # Create your views here.
 
 
@@ -12,11 +12,33 @@ def index(request):
         r_string += p
         r_string += "\n"
 
-
     return HttpResponse("path used in project:\n"+r_string)
 
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
 def test(request):
-    return render(request, 'polls/index.html', {
-        'foo': 'bar',
-    }, content_type='application/xhtml+xml')
+    myVar = 23
+    template = loader.get_template('polls/myHtml/test.html')
+    context = {
+        'latest_question_list': myVar,
+    }
+    return HttpResponse(template.render(context, request))
+
+def test(request):
+    now = mydt.datetime.now()
+    html = "<html>" \
+           "<body>It is now %s.</body>" \
+           "</html>" % now
+    return HttpResponse(html)
